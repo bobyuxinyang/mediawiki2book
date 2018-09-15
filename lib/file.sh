25a69f7f96f338f58d2f326cc368c7e7f482bb98
jk <jk@danjundeMacBook.local>
2018-09-15 22:46:03 +0800

    add shell

---
+++ lib/file.sh
@@ -0,0 +1,20 @@
+
+ #! /bin/sh
+
+
+printfile()
+{
+    srcdir=$1
+    destdir=$2
+    for file in "$srcdir"/*
+    do
+        if [ -d $file ]
+        then
+            printfile $file $destdir
+        else
+            cp $file $destdir
+        fi
+    done
+}
+ 
+printfile $1 $2
